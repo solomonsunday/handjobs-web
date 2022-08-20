2c08bc6a06f6f5ede1b530f59ccfdeb5b2bb7564
Solomon S <schinoyerem007@gmail.com>
2022-08-20 19:32:27 +0100

    Implementing the video call all over again

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import io from "socket.io-client";
 import agent, { API_ROOT } from "./services/agent.service";
-import { UserNotifications } from './store/modules/appNotification';
-import { getConversationList, getConversationWithPartnerId } from './store/modules/chat';
+import { UserNotifications } from "./store/modules/appNotification";
+import {
+  getConversationList,
+  getConversationWithPartnerId,
+} from "./store/modules/chat";
 
-import './styles/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import "./styles/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "antd/dist/antd.css";
+import "font-awesome/css/font-awesome.min.css";
 
-import './App.css';
+import "./App.css";
 
-import AppLoading from './components/AppLoading';
-import AppAlert from 'components/AppAlert';
+import AppLoading from "./components/AppLoading";
+import AppAlert from "components/AppAlert";
 
 const AppRouter = React.lazy(() => import("./routes/app-router"));
 const notificationSocket = io(`${API_ROOT}/notigateway`);
 
-
 function App() {
   const dispatch = useDispatch();
-
 
   useEffect(() => {
     if (agent.Auth.isAuth()) {
       //dispatch(loadApp())
     }
-  }, [dispatch])
+  }, [dispatch]);
 
   const user = agent.Auth.current();
   useEffect(() => {
-    notificationSocket.on('connect', () => {
+    notificationSocket.on("connect", () => {
       if (user && user.id) {
-        notificationSocket.emit('notification_msg_to_server', {
+        notificationSocket.emit("notification_msg_to_server", {
           socketId: user.id,
-        })
+        });
       }
 
       notificationSocket.on("notification_msg_to_client", (data) => {
@@ -52,18 +55,18 @@ function App() {
           dispatch(getConversationList());
         }
       });
-    })
+    });
     return () => notificationSocket.disconnect();
   }, [notificationSocket]);
 
-  const notification = useSelector(state => state.notification);
+  const notification = useSelector((state) => state.notification);
 
   return (
     <React.Suspense fallback={<AppLoading />}>
       <AppAlert notification={notification} />
       <AppRouter />
     </React.Suspense>
-  )
+  );
 }
 
-export default App
+export default App;
